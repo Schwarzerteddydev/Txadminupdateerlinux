29d9a1de7e6b24c859ffb0b2e5ab6bb37ed1b440
Schwarzer_teddy <69168996+Schwarzerteddydev@users.noreply.github.com>
2023-04-14 19:25:21 +0200

    Update Txadminupdateerlinux.sh

--- Txadminupdateerlinux.sh
+++ Txadminupdateerlinux.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 echo 'TxAdmin LINUX AutoUpadater'
-https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/6379-b147842a9107fe9d44c501824f3499445c87dcf3/fx.tar.xz
+wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/6379-b147842a9107fe9d44c501824f3499445c87dcf3/fx.tar.xz
 
 tar xf fx.tar.xz
 
